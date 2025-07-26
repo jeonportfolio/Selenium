7c9223ca3a2f0f1e2e0f884ebb42c57b9ce889f0
jeons <etoile929@naver.com>
2025-07-26 22:00:29 +0900

    영상 순위 자동화

---
+++ viewTracking/app.py
@@ -0,0 +1,38 @@
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller 
+
+chromedriver_autoinstaller.install()
+
+driver = webdriver.Chrome()
+
+query = "김동률"
+
+search_link = f"https://search.naver.com/search.naver?ssc=tab.video.all&where=video&sm=tab_jum&query={query}"
+driver.get(search_link)
+time.sleep(2)
+target_play_link = "https://www.youtube.com/watch?v=6aeZ7CYaNUk"
+link_selector = f'a[href^="{target_play_link}"]'
+PLAY_FOUND = False
+for _ in range(7): # 최대 7번의 영상 순위 찾기 글을 불러온다.
+    try:
+        element = driver.find_element(By.CSS_SELECTOR, link_selector)
+        while True:
+            new_element = element.find_element(By.XPATH, "./..")
+            nowRank = new_element.get_attribute("data-cr-rank")
+            if nowRank != None: 
+                    realRank = int(nowRank) - 20  
+                    print("현재랭크 찾음", realRank)
+                    PLAY_FOUND = True
+                    break
+            print ("현재랭크 못찾음")
+            element = new_element   
+        if PLAY_FOUND: 
+            break     
+    except: 
+        print("타겟 영상을 못찾았습니다 스크롤을 시작합니다.")
+        driver.execute_script("window.scrollBy(0,10000);")
+        time.sleep(3) #  로딩 대기 
+print(f"{realRank} : 타겟 영상의 순위를 찾았습니다.")
+input()
